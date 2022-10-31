7af6c90d2d023cf035654655cad46c921d164a37
İbrahim Enes Taşdan <ibrahimtasdan847@gmail.com>
2022-10-31 23:37:27 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float s1, s2, toplam;
+            s1 = 25
+            s2 = 25;
+            toplam = s1 + s2;
+            Console.WriteLine(toplam);
+            Console.Read();
+
+        }
+    }
+}
